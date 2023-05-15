16c056eb7437f2bb85d0da179312bbcc7ea458b3
donal <donalshortt@donals.eu>
2023-05-15 12:37:17 +0000

    added completions

--- nvim/lua/plugins_config/lsp_config.lua
+++ nvim/lua/plugins_config/lsp_config.lua
@@ -3,9 +3,16 @@ require('mason-lspconfig').setup {
 	ensure_installed = { 'rust_analyzer', 'clangd', 'lua_ls'}
 }
 
-local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lspconfig.rust_analyzer.setup {}
-lspconfig.clangd.setup {}
-lspconfig.lua_ls.setup {}
+require('lspconfig').rust_analyzer.setup {
+	capabilities = capabilities,
+}
 
+require('lspconfig').clangd.setup {
+	capabilities = capabilities,
+}
+
+require('lspconfig').lua_ls.setup {
+	capabilities = capabilities,
+}
