4815a3374bc7129363a28e5afe044c3f316e0c34
donal shortt <donalshortt@donals.eu>
2023-12-14 23:29:36 +0100

    added lspking (symbols in completions)

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,6 +44,7 @@ local plugins = {
 
 	-- lsp stuff
 	'hrsh7th/nvim-cmp',
+	'onsails/lspkind.nvim',
 	'hrsh7th/cmp-nvim-lsp',
 	'L3MON4D3/LuaSnip',
 	'saadparwaiz1/cmp_luasnip',
--- nvim/lua/plugins_config/completions.lua
+++ nvim/lua/plugins_config/completions.lua
@@ -1,4 +1,5 @@
 local cmp = require("cmp")
+local lspkind = require('lspkind')
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -23,4 +24,51 @@ cmp.setup({
 		{ name = 'buffer' },
 		{ name = 'copilot' },
 	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = 'symbol_text', -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			symbol_map = { Copilot = "" }
+		  
+		  -- The function below will be called before any actual modifications from lspkind
+		  -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+		  --before = function (entry, vim_item)
+			--...
+			--return vim_item
+		  --end
+		})
+	}
 })
+
+vim.api.nvim_set_hl(0, "CmpItemKindCopilot", {fg ="#6CC644"})
+
+--[[
+symbol_map = {
+Text = "󰉿",
+Method = "󰆧",
+Function = "󰊕",
+Constructor = "",
+Field = "󰜢",
+Variable = "󰀫",
+Class = "󰠱",
+Interface = "",
+Module = "",
+Property = "󰜢",
+Unit = "󰑭",
+Value = "󰎠",
+Enum = "",
+Keyword = "󰌋",
+Snippet = "",
+Color = "󰏘",
+File = "󰈙",
+Reference = "󰈇",
+Folder = "󰉋",
+EnumMember = "",
+Constant = "󰏿",
+Struct = "󰙅",
+Event = "",
+Operator = "󰆕",
+TypeParameter = "",
+},]]--
+
