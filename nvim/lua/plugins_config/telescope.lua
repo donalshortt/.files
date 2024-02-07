9bda06a4069d075b14dc24d56ff2ea18deb4c067
donal shortt <donalshortt@donals.eu>
2024-02-07 16:12:28 +0100

    telescope fuzzy finder now ignores .gitignore and shows hidden files

--- nvim/lua/plugins_config/telescope.lua
+++ nvim/lua/plugins_config/telescope.lua
@@ -1,1 +1,7 @@
-require('telescope').setup()
+require('telescope').setup {
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--no-ignore", "--hidden", "--glob", "!**/.git/*" }
+		},
+	}
+}
