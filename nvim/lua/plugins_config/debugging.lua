461cd5265cf4321b28294e405cd0cd5b3c285cef
donalshortt <donalshortt@donals.eu>
2025-01-07 22:37:43 +0100

    formatted dapui layout for vertical monitor

--- nvim/lua/plugins_config/debugging.lua
+++ nvim/lua/plugins_config/debugging.lua
@@ -2,91 +2,109 @@ local dap = require("dap")
 
 dap.adapters.gdb = {
   type = "executable",
-  command = "gdb",
-  args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+  command = "rust-gdb",
+  args = { "--interpreter=dap", "--eval-command", "set print pretty on", "-q" }
 }
 
 dap.configurations.rust = {
-  {
-    name = "Launch",
-    type = "gdb",
-    request = "launch",
-    program = function()
-      -- Ensure the project is built before launching
-      os.execute('cargo build')
-      
-      -- Automatically infer the executable from Cargo.toml
-      local handle = io.popen('cargo metadata --no-deps --format-version=1 | jq -r ".packages[0].targets[0].name"')
-      local target_name = handle:read("*a"):gsub("%s+", "")
-      handle:close()
+	{
+		name = "Launch",
+		type = "gdb",
+		request = "launch",
+		program = function()
+			-- Ensure the project is built before launching
+			os.execute('cargo build')
 
-      local executable = vim.fn.getcwd() .. '/target/debug/' .. target_name
-      return executable
-    end,
-    cwd = "${workspaceFolder}",
-    stopAtBeginningOfMainSubprogram = false,
-  },
-  {
-    name = "Select and attach to process",
-    type = "gdb",
-    request = "attach",
-    program = function()
-      -- Ensure the project is built
-      os.execute('cargo build')
+			-- Automatically infer the executable from Cargo.toml
+			local handle = io.popen('cargo metadata --no-deps --format-version=1 | jq -r ".packages[0].targets[0].name"')
+			local target_name = handle:read("*a"):gsub("%s+", "")
+			handle:close()
 
-      -- Automatically infer the executable
-      local handle = io.popen('cargo metadata --no-deps --format-version=1 | jq -r ".packages[0].targets[0].name"')
-      local target_name = handle:read("*a"):gsub("%s+", "")
-      handle:close()
+			local executable = vim.fn.getcwd() .. '/target/debug/' .. target_name
+			return executable
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtBeginningOfMainSubprogram = false,
+	},
+	{
+		name = "Select and attach to process",
+		type = "gdb",
+		request = "attach",
+		program = function()
+			-- Ensure the project is built
+			os.execute('cargo build')
 
-      local executable = vim.fn.getcwd() .. '/target/debug/' .. target_name
-      return executable
-    end,
-    pid = function()
-      local name = vim.fn.input('Executable name (filter): ')
-      return require("dap.utils").pick_process({ filter = name })
-    end,
-    cwd = '${workspaceFolder}'
-  },
-  {
-    name = 'Attach to gdbserver :1234',
-    type = 'gdb',
-    request = 'attach',
-    target = 'localhost:1234',
-    program = function()
-      -- Ensure the project is built
-      os.execute('cargo build')
+			-- Automatically infer the executable
+			local handle = io.popen('cargo metadata --no-deps --format-version=1 | jq -r ".packages[0].targets[0].name"')
+			local target_name = handle:read("*a"):gsub("%s+", "")
+			handle:close()
 
-      -- Automatically infer the executable
-      local handle = io.popen('cargo metadata --no-deps --format-version=1 | jq -r ".packages[0].targets[0].name"')
-      local target_name = handle:read("*a"):gsub("%s+", "")
-      handle:close()
+			local executable = vim.fn.getcwd() .. '/target/debug/' .. target_name
+			return executable
+		end,
+		pid = function()
+			local name = vim.fn.input('Executable name (filter): ')
+			return require("dap.utils").pick_process({ filter = name })
+		end,
+		cwd = '${workspaceFolder}'
+	},
+	{
+		name = 'Attach to gdbserver :1234',
+		type = 'gdb',
+		request = 'attach',
+		target = 'localhost:1234',
+		program = function()
+			-- Ensure the project is built
+			os.execute('cargo build')
 
-      local executable = vim.fn.getcwd() .. '/target/debug/' .. target_name
-      return executable
-    end,
-    cwd = '${workspaceFolder}'
-  },
+			-- Automatically infer the executable
+			local handle = io.popen('cargo metadata --no-deps --format-version=1 | jq -r ".packages[0].targets[0].name"')
+			local target_name = handle:read("*a"):gsub("%s+", "")
+			handle:close()
+
+			local executable = vim.fn.getcwd() .. '/target/debug/' .. target_name
+			return executable
+		end,
+		cwd = '${workspaceFolder}'
+	},
 }
 
 local opts = {
 	layouts = {
 		{
 			elements = {
-				{ id = "scopes", size = 0.25 },
-				{ id = "stacks", size = 0.25 },
-				{ id = "breakpoints", size = 0.20 },
-				{ id = "watches", size = 0.30 },
+				{ id = "breakpoints", size = 1 },
 			},
-			position = "left",
-			size = 35,
+			position = "top",
+			size = 3,
+		},
+		{
+			elements = {
+				{ id = "watches", size = 1 },
+			},
+			position = "top",
+			size = 5,
+		},
+		{
+			elements = {
+				{ id = "scopes", size = 1 },
+			},
+			position = "top",
+			size = 10,
+		},
+		{
+			elements = {
+				{ id = "console", size = 1 },
+			},
+			position = "bottom",
+			size = 5,
 		},
 		{
 			elements = {
 				{ id = "repl", size = 1 },
 			},
 			position = "bottom",
-			size = 15,
+			size = 9,
 		},
 	},
 }
@@ -95,14 +113,14 @@ require("dapui").setup(opts)
 local dapui = require("dapui")
 
 dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
+	dapui.close()
 end
