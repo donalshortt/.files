ec88e6b53c209ed0b62f9c72808f87c92e730272
donalshortt <donalshortt@donals.eu>
2025-04-03 19:33:34 +0200

    make telescope layout vertical

--- nvim/lua/plugins_config/telescope.lua
+++ nvim/lua/plugins_config/telescope.lua
@@ -1,4 +1,8 @@
-require('telescope').setup {
+require('telescope').setup{
+    defaults = {
+      layout_strategy = 'vertical',
+      layout_config = { height = 0.95 },
+    },
 	pickers = {
 		find_files = {
 			find_command = { "rg", "--files", "--no-ignore", "--hidden", "--glob", "!**/.git/*" }
